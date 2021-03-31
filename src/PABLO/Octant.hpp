51b88287085e89913af252880c3d717613d8960d
James Sharpe <james.sharpe@zenotech.com>
2021-03-31 12:43:34 +0000

    Optimise layout of Octant members to reduce memory usage

--- src/PABLO/Octant.hpp
+++ src/PABLO/Octant.hpp
@@ -136,14 +136,14 @@ private:
     uint32_t                        m_x;            /**< Coordinate x */
     uint32_t                        m_y;            /**< Coordinate y */
     uint32_t                        m_z;            /**< Coordinate z (2D case = 0)*/
-    uint8_t                         m_level;        /**< Refinement level (0=root) */
-    int8_t                          m_marker;       /**< Set for Refinement(m>0) or Coarsening(m<0) |m|-times */
     std::bitset<INFO_ITEM_COUNT>    m_info;         /**< -Info[0..5]: true if 0..5 face is a boundary face [bound] \n
                                                          -Info[6..11]: true if 0..6 face is a process boundary face [pbound] \n
                                                          -Info[12/13]: true if octant is new after refinement/coarsening \n
                                                          -Info[14]   : true if balancing is required for this octant \n
                                                          -Info[15]   : Aux */
-    uint8_t                         m_dim;          /**< Dimension of octant (2D/3D) */
+    int8_t                          m_marker;       /**< Set for Refinement(m>0) or Coarsening(m<0) |m|-times */
+    uint8_t                         m_level : 6;        /**< Refinement level (0=root) */
+    uint8_t                         m_dim : 2;          /**< Dimension of octant (2D/3D) */
     int                             m_ghost;        /**< Ghost specifier:\n
                                                          -1 : internal, \n
                                                           0 : ghost in the 0-th layer of the halo, \n
